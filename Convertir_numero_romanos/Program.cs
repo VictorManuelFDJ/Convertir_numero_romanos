ebc5c3a5c3022dd8f27c10335b1a1da808876bbe
Victor Manuel Frias De Jesús <victor_frias1@ucne.edu.do>
2025-09-02 00:54:01 -0400

    En esta parte esta la estrututa base del programa pero el numero lll romano sale en -2

--- Convertir_numero_romanos/Program.cs
+++ Convertir_numero_romanos/Program.cs
@@ -2,6 +2,50 @@
 {
     public class ConvertirNumeroRomanos
     {
-      
+        private static int ValorRomano(char simbolo)
+        {
+            switch (simbolo)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+
+            }
+        }
+
+        public static int ConvertirADecimales(string numeroRomano) {
+            int resultado = 0;
+            for(int i = 0; i < numeroRomano.Length; i++) { 
+                int valorActual = ValorRomano(numeroRomano[i]);
+
+                if (i + 1 < numeroRomano.Length)
+                {
+                    int valorSiguiente = (numeroRomano[i + 1]);
+
+                    if (valorActual < valorSiguiente)
+                    {
+                        resultado -= valorActual;
+
+                    }
+                    else { 
+                        resultado += valorActual;
+                    }
+                }
+            }
+            return resultado;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string numeroRomano1 = "III";
+            int decimal1 = ConvertirADecimales(numeroRomano1);
+            Console.WriteLine($"El número romano {numeroRomano1} es {decimal1} en decimal.");
+        }
     }
 }
